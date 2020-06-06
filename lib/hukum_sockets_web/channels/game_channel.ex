d9b9a24bd4d6426c167bbe3225ff22e98f3f58db
Nikhil Sonnad <nikhil@nsnnd.com>
2020-06-06 17:14:57 +0530

    confirm game upon teams being [2,2]

--- lib/hukum_sockets_web/channels/game_channel.ex
+++ lib/hukum_sockets_web/channels/game_channel.ex
@@ -25,9 +25,34 @@ defmodule HukumSocketsWeb.GameChannel do
      GameList.set_open(socket.assigns.game_name, false)
     end
 
-    broadcast(socket, "game_state", %{game: get_player_game(socket.assigns.game_name)})
+    broadcast_game(socket)
     push socket, "presence_state", Presence.list(socket)
     {:noreply, socket}
+  end
+
+  @impl true
+  def handle_in("choose_team", %{"team" => team}, socket) do
+    case HukumEngine.choose_team(
+      via(socket.assigns.game_name),
+      socket.assigns.user_name,
+      team
+    ) do
+      {:ok, %{team_counts: [2, 2] }} ->
+        HukumEngine.confirm_teams(via(socket.assigns.game_name))
+        broadcast_game(socket)
+        {:reply, :ok, socket}
+      {:ok, _ } ->
+        broadcast_game(socket)
+        {:reply, :ok, socket}
+      {:error, :team_full} ->
+        {:reply, {:error, %{reason: "team_full" }}, socket}
+    end
+  end
+
+  # TODO: randomly assign teams after a period of no choosing
+
+  defp broadcast_game(socket) do
+    broadcast(socket, "game_state", %{game: get_player_game(socket.assigns.game_name)})
   end
 
   defp get_player_game(game_name) do
@@ -47,7 +72,7 @@ defmodule HukumSocketsWeb.GameChannel do
 
   defp assign_game(socket, game_name, user_name) do
     socket
-    |> assign(:user_name, user_name)
+    |> assign(:user_name, String.to_atom(user_name))
     |> assign(:game_name, game_name)
   end
 
--- test/hukum_sockets_web/channels/game_channel_test.exs
+++ test/hukum_sockets_web/channels/game_channel_test.exs
@@ -54,4 +54,35 @@ defmodule HukumSocketsWeb.GameChannelTest do
     assert game5 == {:error, %{reason: "game_full"}}
   end
 
+  test "after everyone joins and chooses a team we move to :call_or_pass phase" do
+    GameList.add_game("test-game3", %{"user_name" => "player1", "private" => false})
+    HukumEngine.new_game("test-game3")
+
+    {:ok, _, socket1} =
+      HukumSocketsWeb.UserSocket
+      |> socket("user_name", %{user_name: :assign})
+      |> subscribe_and_join(HukumSocketsWeb.GameChannel, "game:test-game3", %{user_name: "player1"})
+
+    {:ok, _, socket2} =
+      HukumSocketsWeb.UserSocket
+      |> socket("user_name", %{user_name: :assign})
+      |> subscribe_and_join(HukumSocketsWeb.GameChannel, "game:test-game3", %{user_name: "player2"})
+
+    {:ok, _, socket3} =
+      HukumSocketsWeb.UserSocket
+      |> socket("user_name", %{user_name: :assign})
+      |> subscribe_and_join(HukumSocketsWeb.GameChannel, "game:test-game3", %{user_name: "player3"})
+
+    {:ok, _, socket4} =
+      HukumSocketsWeb.UserSocket
+      |> socket("user_name", %{user_name: :assign})
+      |> subscribe_and_join(HukumSocketsWeb.GameChannel, "game:test-game3", %{user_name: "player4"})
+
+    push socket1, "choose_team", %{team: 1}
+    push socket2, "choose_team", %{team: 1}
+    push socket3, "choose_team", %{team: 2}
+    push socket4, "choose_team", %{team: 2}
+
+    assert_broadcast("game_state", %{:game => %{"stage": :call_or_pass}})
+  end
 end
