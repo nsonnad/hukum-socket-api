145fc56844db459050c988d0174dcf16d5774ae0
Nikhil Sonnad <nikhil@nsnnd.com>
2020-06-09 22:48:42 +0530

    againagain

--- lib/hukum_sockets_web/endpoint.ex
+++ lib/hukum_sockets_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule HukumSocketsWeb.Endpoint do
   ]
 
   socket "/socket", HukumSocketsWeb.UserSocket,
-    websocket: true,
+    websocket: [ check_origin: [ "//hukum.gigalixirapp.com" ]],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
