48b3b775eef3793eace53ba5b6301fa8ea36ca50
Nikhil Sonnad <nikhil@nsnnd.com>
2020-06-09 20:26:53 +0530

    releases configuration

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,11 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :hukum_sockets, HukumSocketsWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  load_from_system_env: true,
+  http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
+  server: true, # Without this line, your app will not start the web server!
+  secret_key_base: "${SECRET_KEY_BASE}",
+  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
